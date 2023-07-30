f209fcccb15671f93df209f55c34b76c1b313f99
UDAY SUTARIA <98198229+udaysutaria20@users.noreply.github.com>
2023-07-30 10:07:48 +0530

    Create Tasak-6.js

---
+++ PRACTICAL-4/Tasak-6.js
@@ -0,0 +1,6 @@
+import { dirname, basename } from 'node:path'; 
+const eg='D:/Files/Semester 5/FSWD/Node js/Week 
+4/task2.js'; console.log(`Path: ${eg}`); 
+console.log("Directory name: 
+"+dirname(eg)); console.log("Base 
+name: "+basename(eg)); 
